651f072e415e194e907b8df4572370064c05d970
kadoka <tomiya3499@gmail.com>
2026-09-16 22:32:24 +0900

    Test Common IR and comment adapter contracts separately

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -1,14 +1,22 @@
 from Src.analyzers.ir import ModuleIR
-from Src.languages import LanguageAdapter
+from Src.languages import CSharpAdapter, LanguageAdapter
 from Src.languages.python import PythonLanguageAdapter
+from Src.models.comments import ParsedSource
+from Src.process.contracts import CommentAdapter
 from Src.renderers import Renderer
 from Src.renderers.mermaid_call_graph import render_call_graph
 
 
-def test_language_adapter_contract_accepts_common_ir_result() -> None:
-    adapter: LanguageAdapter[ModuleIR] = PythonLanguageAdapter()
+def test_language_adapter_contract_returns_common_ir() -> None:
+    adapter: LanguageAdapter = PythonLanguageAdapter()
     result = adapter.parse("value = 1\n")
     assert isinstance(result, ModuleIR)
+
+
+def test_comment_adapter_contract_is_separate_from_common_ir_adapter() -> None:
+    adapter: CommentAdapter = CSharpAdapter()
+    result = adapter.parse("class Sample {}\n")
+    assert isinstance(result, ParsedSource)
 
 
 def test_renderer_contract_describes_text_output() -> None:
