d2f72e2558b559b27962d8f54fbae4e315ae40ce
kadoka <tomiya3499@gmail.com>
2026-09-16 22:26:08 +0900

    Export renderer-neutral generator APIs

--- Src/generators/__init__.py
+++ Src/generators/__init__.py
@@ -1,16 +1,26 @@
 """Logical output generators."""
 
-from .call_graph import generate_call_graph_mermaid
+from .call_graph import build_call_graph, build_call_graph_bundle
 from .comment_backend import CommentTextBackend, RuleBasedCommentBackend
 from .comment_generator import CommentGenerator, UnsupportedLanguageError
 from .comments import CommentCandidate, RuleBasedCommentGenerator
 from .flowchart import generate_flowchart
-from .responsibility import ResponsibilityRow, partitions, rows, to_csv, to_markdown
+from .responsibility import build_responsibility_table_bundle, partitions, rows
+from Src.models.responsibility import ResponsibilityRow, ResponsibilityTable
 
 __all__ = [
-    "CommentCandidate", "CommentTextBackend", "CommentGenerator",
-    "RuleBasedCommentBackend", "RuleBasedCommentGenerator", "ResponsibilityRow",
-    "rows", "to_csv", "to_markdown", "partitions", "UnsupportedLanguageError",
-    "generate_call_graph_mermaid", "generate_flowchart",
+    "CommentCandidate",
+    "CommentTextBackend",
+    "CommentGenerator",
+    "RuleBasedCommentBackend",
+    "RuleBasedCommentGenerator",
+    "ResponsibilityRow",
+    "ResponsibilityTable",
+    "rows",
+    "partitions",
+    "build_responsibility_table_bundle",
+    "build_call_graph",
+    "build_call_graph_bundle",
+    "UnsupportedLanguageError",
+    "generate_flowchart",
 ]
-
