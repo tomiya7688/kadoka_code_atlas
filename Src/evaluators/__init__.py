d13d9005c545352e9fcbb98fca523569dc9c277f
kadoka <tomiya3499@gmail.com>
2026-09-16 12:43:02 +0900

    設計品質Evaluatorを公開

--- Src/evaluators/__init__.py
+++ Src/evaluators/__init__.py
@@ -1,5 +1,11 @@
 """Design evaluators for normalized analysis models."""
 
 from .ci import CIEvaluationFinding, evaluate_ci
+from .design_quality import DesignQualityThresholds, evaluate_design_quality
 
-__all__ = ["CIEvaluationFinding", "evaluate_ci"]
+__all__ = [
+    "CIEvaluationFinding",
+    "DesignQualityThresholds",
+    "evaluate_ci",
+    "evaluate_design_quality",
+]
