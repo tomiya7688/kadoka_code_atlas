725b51c0c64946171fd136e1ab1dadeed64e41ef
kadoka <tomiya3499@gmail.com>
2026-09-16 22:26:23 +0900

    Export responsibility renderers

--- Src/renderers/__init__.py
+++ Src/renderers/__init__.py
@@ -5,5 +5,14 @@ from .flowchart import render_flowchart
 from .mermaid_activity import render_activity_flow
 from .mermaid_call_graph import render_call_graph
 from .mermaid_ci import render_ci_workflow
+from .responsibility_table import render_responsibility_csv, render_responsibility_markdown
 
-__all__ = ["Renderer", "render_activity_flow", "render_call_graph", "render_ci_workflow", "render_flowchart"]
+__all__ = [
+    "Renderer",
+    "render_activity_flow",
+    "render_call_graph",
+    "render_ci_workflow",
+    "render_flowchart",
+    "render_responsibility_csv",
+    "render_responsibility_markdown",
+]
