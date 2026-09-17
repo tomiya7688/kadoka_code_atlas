f69a833d1f9cb5952b3623f2bfd38aab8a866587
kadoka <tomiya3499@gmail.com>
2026-09-18 07:52:33 +0900

    Export CI provider adapter contract

--- Src/analyzers/ci/__init__.py
+++ Src/analyzers/ci/__init__.py
@@ -1,14 +1,18 @@
 """CI workflow analyzers and language-neutral models."""
 
+from .contracts import CIProviderAdapter
 from .github_actions import parse_github_actions
+from .github_actions_adapter import GitHubActionsAdapter
 from .source_links import collect_ci_path_hints
 from Src.models.ci import CIJob, CIPathHint, CIStep, CIWorkflow
 
 __all__ = [
     "CIJob",
     "CIPathHint",
+    "CIProviderAdapter",
     "CIStep",
     "CIWorkflow",
+    "GitHubActionsAdapter",
     "collect_ci_path_hints",
     "parse_github_actions",
 ]
